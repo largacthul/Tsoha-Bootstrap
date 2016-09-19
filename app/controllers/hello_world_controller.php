5caa7a1c253fd5ef1cd8d74d01d87f5141e9bdea
largacthul <lari.grohn@gmail.com>
2016-09-19 18:44:10 +0300

    dokumentaation päivitys - tietokohteet, tietokantakaavio

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,26 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
+    }
+
+    public static function job_list(){
+      //Listaussivu
+      View::make('suunnitelmat/job_list.html');
+    }
+
+    public static function job_edit(){
+      //Muokkaus
+      View::make('suunnitelmat/job_edit.html');
+    }
+
+    public static function login(){
+      //Kirjautuminen
+      View::make('suunnitelmat/login.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,15 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/job_list', function() {
+    HelloWorldController::job_list();
+  });
+
+  $routes->get('/job_edit', function() {
+    HelloWorldController::job_edit();
+  });
+
+  $routes->get('/login', function() {
+    HelloWorldController::login();
+  });
