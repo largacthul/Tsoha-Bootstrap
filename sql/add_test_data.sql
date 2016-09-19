778888ca569959182b868772d2cc760a777d196e
largacthul <lari.grohn@gmail.com>
2016-09-19 23:47:39 +0300

    taulujen luonti, poisto, testidata

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,16 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+-- NoteOwner testidata
+INSERT INTO NoteOwner (tunnus, nimi, kuvaus, salasana) VALUES ('NoteMaster', 'Paavo Paavolainen', 'Muistiinpanojen harrastaja nr 1', 'password1');
+INSERT INTO NoteOwner (tunnus, nimi, kuvaus, salasana) VALUES ('Muistiinpanija', 'Aatu Askare', 'Satunnainen käyttäjä', 'salasana');
+-- Note testidata
+INSERT INTO Note (otsikko, kuvaus) VALUES ('Auton katsastus', 'Auto pitää muistaa viedä katsastettavaksi!');
+INSERT INTO Note (otsikko, kuvaus) VALUES ('Konsertti Tavastialla', 'Duuniporukan kanssa marraskuussa katsomaan Popedaa Tavastialle. Muista ostaa liput!');
+INSERT INTO Note (otsikko, kuvaus) VALUES ('Vessan lavuaari', 'Lavuaari vetää huonosti. Osta Kodin putkimiestä.');
+-- Label testidata
+INSERT INTO Label (nimi) VALUES ('Työ');
+INSERT INTO Label (nimi) VALUES ('Byrokratia');
+INSERT INTO Label (nimi) VALUES ('Tapahtumat');
+INSERT INTO Label (nimi) VALUES ('Koti');
+-- Priority testidata
+INSERT INTO Priority (prioriteetti) VALUES ('Kriittinen!');
+INSERT INTO Priority (prioriteetti) VALUES ('Tärkeä');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,43 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE NoteOwner (
+  id SERIAL PRIMARY KEY,
+  tunnus varchar(30) NOT NULL,
+  nimi varchar(30) NOT NULL,
+  kuvaus varchar(120),
+  salasana varchar(30) NOT NULL
+);
+
+CREATE TABLE Note (
+  id SERIAL PRIMARY KEY,
+  noteowner_id INTEGER REFERENCES NoteOwner(id),
+  otsikko varchar(50) NOT NULL,
+  kuvaus varchar(200)
+);
+
+CREATE TABLE NoteViewer (
+  noteowner_id INTEGER REFERENCES NoteOwner(id),
+  note_id INTEGER REFERENCES Note(id)
+);
+
+CREATE TABLE Label (
+  id SERIAL PRIMARY KEY,
+  nimi varchar(50) NOT NULL
+);
+
+CREATE TABLE NoteLabel (
+  note_id INTEGER REFERENCES Note(id),
+  label_id INTEGER REFERENCES Label(id)
+);
+
+CREATE TYPE prio_type AS ENUM (
+  'Kriittinen!',
+  'Tärkeä',
+  'Normaali',
+  'Ei tärkeä'
+);
+
+CREATE TABLE Priority (
+  id SERIAL PRIMARY KEY,
+  note_id INTEGER REFERENCES Note(id),
+  prioriteetti prio_type DEFAULT 'Normaali' 
+);
